cf74c97f1048c17eada74d1467fba8ad68aee3c8
Ankush.Yerawar <ankushyerawar@gmail.com>
2020-12-02 17:36:19 +0530

    Launch Screen Design

--- app/src/main/java/com/ankushyerawar/newsletter/ui/fragment/LaunchFragment.kt
+++ app/src/main/java/com/ankushyerawar/newsletter/ui/fragment/LaunchFragment.kt
@@ -1,20 +1,22 @@
 package com.ankushyerawar.newsletter.ui.fragment
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.ankushyerawar.newsletter.R
 import com.ankushyerawar.newsletter.ui.viewmodel.SplashViewModel
-import kotlinx.android.synthetic.main.fragment_launch.*
+import com.ankushyerawar.newsletter.utils.TypeWriter
 
 class LaunchFragment : Fragment() {
 
     private val mViewModel: SplashViewModel by viewModels()
+
+    private lateinit var mTextView: TypeWriter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,11 +31,24 @@ class LaunchFragment : Fragment() {
 
         //If the User is using this app for the first time don't show him launch screen
         if (!mViewModel.showSplashFragment) {
-            Navigation.findNavController(view).navigate(LaunchFragmentDirections.actionNavigationLaunchToNavigationHeadline())
+            Navigation.findNavController(view)
+                .navigate(LaunchFragmentDirections.actionNavigationLaunchToNavigationHeadline())
         }
 
         //If this is the first time a User is using the app show him splash screen
         mViewModel.showSplashFragment = true
+
+        mTextView = view.findViewById(R.id.title_label)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //Animate TextView
+        mTextView.setCharacterDelay(150);
+        mTextView.animateText("News Letter");
+
     }
 
 
