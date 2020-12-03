89e40ad33a82efc25e31b6c37f0abd40de0df30d
Ankush.Yerawar <ankushyerawar@gmail.com>
2020-12-03 11:23:43 +0530

    Added Data Binding

--- app/src/main/java/com/ankushyerawar/newsletter/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/ankushyerawar/newsletter/ui/fragment/SplashFragment.kt
@@ -5,32 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.TextView
-import android.widget.ViewFlipper
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
-import com.airbnb.lottie.LottieAnimationView
 import com.ankushyerawar.newsletter.R
 import com.ankushyerawar.newsletter.data.model.Splash
+import com.ankushyerawar.newsletter.databinding.FragmentSplashBinding
 import com.ankushyerawar.newsletter.ui.viewmodel.SplashViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SplashFragment : Fragment() {
 
     private val mViewModel: SplashViewModel by viewModels()
 
-    private lateinit var mViewFlipper: ViewFlipper
-    private lateinit var mLottieView: LottieAnimationView
-    private lateinit var mTxtHeader: TextView
-    private lateinit var mTxtDesc: TextView
+    private lateinit var mBinding: FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
+        mBinding.viewModel = mViewModel
+        mViewModel.splashList = createSplashList()
+        mBinding.splash = mViewModel.splashList?.get(mViewModel.showNext)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,38 +37,30 @@ class SplashFragment : Fragment() {
 
         //If the User is using this app for the second time don't show him splash screen
         if (mViewModel.showSplashFragment) {
-            Navigation.findNavController(view).navigate(SplashFragmentDirections.actionNavigationSplashToNavigationLaunch())
+            Navigation.findNavController(view)
+                .navigate(SplashFragmentDirections.actionNavigationSplashToNavigationLaunch())
         }
 
         setUpViews(view)
     }
 
     private fun setUpViews(view: View) {
-        mViewFlipper = view.findViewById(R.id.view_flipper)
-        mLottieView = view.findViewById(R.id.lottie_splash)
-        mTxtHeader = view.findViewById(R.id.txt_header)
-        mTxtDesc = view.findViewById(R.id.txt_desc)
+        mBinding.viewFlipper.inAnimation =
+            AnimationUtils.loadAnimation(activity, R.anim.slide_in_right)
+        mBinding.viewFlipper.outAnimation =
+            AnimationUtils.loadAnimation(activity, R.anim.slide_out_left)
 
-        val list = createSplashList()
+        mBinding.fabNext.setOnClickListener {
 
-        mViewFlipper.inAnimation = AnimationUtils.loadAnimation(activity, R.anim.slide_in_right)
-        mViewFlipper.outAnimation = AnimationUtils.loadAnimation(activity, R.anim.slide_out_left)
+            if (mViewModel.showNext < mViewModel.splashList!!.size - 1) {
+                mBinding.viewFlipper.showNext()
 
-        var index = 0
+                mViewModel.showNext += 1
 
-        val fabButton: FloatingActionButton = view.findViewById(R.id.fab_next)
-        fabButton.setOnClickListener {
+                mBinding.splash = mViewModel.splashList?.get(mViewModel.showNext)
+                mBinding.lottieSplash.setAnimation(mBinding.splash!!.resId)
+                mBinding.lottieSplash.playAnimation()
 
-            if (index < 2) {
-                mViewFlipper.showNext()
-
-                mLottieView.setAnimation(list[index].resId)
-                mLottieView.playAnimation()
-
-                mTxtHeader.text = list[index].titleHeader
-                mTxtDesc.text = list[index].titleDesc
-
-                index++
             } else {
                 Navigation.findNavController(view)
                     .navigate(SplashFragmentDirections.actionNavigationSplashToNavigationLaunch())
@@ -80,6 +71,11 @@ class SplashFragment : Fragment() {
 
     private fun createSplashList(): List<Splash> {
         return listOf(
+            Splash(
+                R.raw.splash_one,
+                resources.getString(R.string.title_header_one),
+                resources.getString(R.string.title_desc_one)
+            ),
             Splash(
                 R.raw.splash_two,
                 resources.getString(R.string.title_header_two),
--- app/src/main/java/com/ankushyerawar/newsletter/ui/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/ankushyerawar/newsletter/ui/viewmodel/SplashViewModel.kt
@@ -3,16 +3,20 @@ package com.ankushyerawar.newsletter.ui.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.ankushyerawar.newsletter.application.BaseApp
+import com.ankushyerawar.newsletter.data.model.Splash
 import com.ankushyerawar.newsletter.utils.AppConstants
 import com.ankushyerawar.newsletter.utils.SharedPreferenceHelper
 
 class SplashViewModel(application: Application) : AndroidViewModel(application) {
 
+    //get required fields
     private val mSharedPreferences = (application as (BaseApp)).preferences
 
+    //check if Network is available
     val isInternetAvailable: Boolean
         get() { return SharedPreferenceHelper.getBoolean(mSharedPreferences, AppConstants.INTERNET, false)}
 
+    //Show Splash screen only once
     var showSplashFragment: Boolean
         set(value) {
             SharedPreferenceHelper.setBoolean(mSharedPreferences, AppConstants.SPLASH_SCREEN, value)
@@ -22,4 +26,14 @@ class SplashViewModel(application: Application) : AndroidViewModel(application) 
             AppConstants.SPLASH_SCREEN,
             false
         )
+
+    //Data to be shown on Splash screen
+    var showNext: Int = 0
+
+    var splashList: List<Splash> ?= null
+    set(value) {
+        field = value
+    }
+    get() = field
+
 }
